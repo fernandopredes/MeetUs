48ef981387f454a9986d7eaa2a763bedeef44f87
fernando predes <fernando_predes@hotmail.com>
2022-12-19 19:08:15 -0300

    header component ok

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+  import Header from "./lib/Header.svelte";
 
+  const meetups: Array<Object>=[
+    {
+      id:'m1',
+      title:'Coding Camp',
+      subtitle: 'Learn to Code',
+      description: 'In this meetup, we will have new experts',
+      imageUrl:'https://poketouch.files.wordpress.com/2022/11/hammer_pokemon_tinkaton.png',
+      address:'3744 E Chapman Ave',
+      conctactEmail:'test@test.com'
+    },
+    {
+      id:'m2',
+      title:'Coding Camp',
+      subtitle: 'Learn to Code part 2',
+      description: 'In this meetup, we will MORE new experts',
+      imageUrl:'https://poketouch.files.wordpress.com/2017/11/okemon_laptop_wrist_rest_mew_plushie_and_cushion.jpg',
+      address:'3744 E Chapman Ave',
+      conctactEmail:'test@test.com'
+    }
+  ]
 </script>
-
-<main>
-
-  <h1>Meet Us</h1>
-
-
-</main>
 
 <style>
 
 </style>
+
+<Header />
